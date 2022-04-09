98393ae9391077f61afd63580e817fc5d8cc091d
kazukiDAO <kazuki480@gmail.com>
2022-04-09 23:35:40 +0900

    jar生成時にエラーが出るため修正

--- src/test/java/com/example/demo/SpringSampleApplicationTests.java
+++ src/test/java/com/example/demo/SpringSampleApplicationTests.java
@@ -3,7 +3,9 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.app.SpringMainApplication;
+
+@SpringBootTest(classes = SpringMainApplication.class)
 class SpringSampleApplicationTests {
 
 	@Test
